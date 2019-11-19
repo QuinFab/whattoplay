7d117b1326a2f7893f7143cd624f2c57e6758be2
QuinFab <fabio.corrado@quinscape.de>
2019-11-19 15:24:33 +0100

    Bootstrap outcluded und einloggen.html eingefügt

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,12 @@
-function registrieren(){
-    alert("Die Funktion gibts noch nicht, hör auf da drauf zu klicken!")
+function Platzhalter(){
+    alert("Die Funktion gibts noch nicht, hör auf da drauf zu klicken! Später gibts dann was sinnvolles")
+}
+function redirectZuRegistrieren(){
+    window.location.href = "registrieren.html";
+}
+function redirectEinloggen(){
+    window.location.href = "einloggen.html";
+}
+function registrierungBestätigen() {
+
 }
