7a2bb699629208fd31bef37a385f2baddce94cb3
Hsyn-Krnz <huseyin.kurnaz@itc-studenten.de>
2019-11-20 15:12:19 +0100

    Files renamed from html to php

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,10 @@ function Platzhalter(){
     alert("Die Funktion gibts noch nicht, hör auf da drauf zu klicken! Später gibts dann was sinnvolles")
 }
 function redirectZuRegistrieren(){
-    window.location.href = "registrieren.html";
+    window.location.href = "registrieren.php";
 }
 function redirectEinloggen(){
-    window.location.href = "einloggen.html";
+    window.location.href = "einloggen.php";
 }
 function registrierungBestätigen() {
 
