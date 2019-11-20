88a78d385e73d214a2f9c3586622aa50b8e6e0b7
Hsyn-Krnz <huseyin.kurnaz@itc-studenten.de>
2019-11-20 21:16:20 +0100

    iba.sql deleted input box configuration for better insert into db

--- registrieren.php
+++ registrieren.php
@@ -1,3 +1,25 @@
+
+<?php
+
+if (isset($_POST['Username'])) {
+// VERARBEITUNG
+$db = new PDO(
+        'mysql:host=localhost;dbname=iba',
+        'root',
+        ''
+);
+
+$query = 'INSERT INTO user (user_id, email, password) VALUES (:user_id, :email, :password)';
+$preparedStmt = $db->prepare($query);
+
+$preparedStmt->bindValue(':user_id', $_POST['Username']);
+$preparedStmt->bindValue(':email', $_POST['UserMail']);
+$preparedStmt->bindValue(':password', password_hash($_POST['Passwort'], PASSWORD_BCRYPT, ['cost' => 11]));
+        $res = $preparedStmt->execute();
+    header('Location: einloggen.php');
+}
+?>
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -14,19 +36,19 @@
 
 <form method="post">
         <label for="Username">Benutzername </label>
-        <input id="Username" name="Username" type="text" placeholder="z.B. MaxMaster3314">
+        <input id="Username" name="Username" type="text" placeholder="z.B. MaxMaster3314" minlength="3" maxlength="16" required>
         <br>
         <label for="Passwort">Passwort </label>
-        <input id="Passwort" name="Passwort" type="password" placeholder="Passwort">
+        <input id="Passwort" name="Passwort" type="password" placeholder="Passwort" minlength="8" required>
          <br>
         <label for="UserMail">Email-Adresse </label>
-        <input id="UserMail" name="UserMail" type="email" placeholder="z.B. meineMail@web.de">
+        <input id="UserMail" name="UserMail" type="email" placeholder="z.B. meineMail@web.de" required>
          <br>
         <label>Email-Adresse bestätigen </label>
-        <input type="email" placeholder="z.B. meineMail@web.de">
+        <input type="email" placeholder="z.B. meineMail@web.de" required>
 
     <button id="registrieren" type="submit" onclick="Platzhalter()" >Registrierung bestätigen</button>
-<!-- HIER NOCH PHP FUNKTION REINKNALLEN -->
+
 </form>
 
 </body>
