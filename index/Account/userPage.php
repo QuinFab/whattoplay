2d81bf910a42c578e41b7ada9898f4bc37fc44ed
QuinFab <fabio.corrado@quinscape.de>
2019-11-21 09:03:05 +0100

    Ordnerstruktur hinzugefügt

--- index/Account/userPage.php
+++ index/Account/userPage.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <title>Account</title>
 
-    <link rel="stylesheet" type="text/css" href="css/custom.css"/>
-    <script src="js/custom.js"></script>
+    <link rel="stylesheet" type="text/css" href="../../css/custom.css"/>
+    <script src="../../js/custom.js"></script>
 </head>
 <body>
 <h1> Benutzerkonto </h1>
