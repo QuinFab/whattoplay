bae9fc51499a73c4e2acf843cc6763d2f0a89a72
Hsyn-Krnz <huseyin.kurnaz@itc-studenten.de>
2019-11-22 11:04:53 +0100

    einfach ignorieren aber nicht löschen

--- index/AccountErstellen/registrieren.php
+++ index/AccountErstellen/registrieren.php
@@ -1,20 +1,19 @@
 <?php
-if (isset($_POST['Username'])) {
+if (isset($_POST['user_id'])) {
 // VERARBEITUNG
-$db = new PDO(
+    $db = new PDO(
         'mysql:host=localhost;dbname=iba',
         'root',
         ''
-);
+    );
 
-$query = 'INSERT INTO user (user_id, email, password) VALUES (:user_id, :email, :password)';
-$preparedStmt = $db->prepare($query);
-
-$preparedStmt->bindValue(':user_id', $_POST['Username']);
-$preparedStmt->bindValue(':email', $_POST['UserMail']);
-$preparedStmt->bindValue(':password', password_hash($_POST['Passwort'], PASSWORD_BCRYPT, ['cost' => 12]));
-        $res = $preparedStmt->execute();
-    header('Location: einloggen.php');
+            $query = 'INSERT INTO user (user_id, email, password) VALUES (:user_id, :email, :Password)';
+            $preparedStmt = $db->prepare($query);
+                $preparedStmt->bindValue(':Password', password_hash($_POST['Password'], PASSWORD_BCRYPT, ['cost' => 12]));
+                $preparedStmt->bindValue(':Password2', password_hash($_POST['Password2'], PASSWORD_BCRYPT, ['cost' => 12]));
+            $preparedStmt->bindValue(':user_id', $_POST['Username']);
+            $preparedStmt->bindValue(':email', $_POST['email']);
+            $res = $preparedStmt->execute();
 }
 ?>
 <!DOCTYPE html>
@@ -32,22 +31,23 @@ $preparedStmt->bindValue(':password', password_hash($_POST['Passwort'], PASSWORD
 <p> Jetzt die relevanten Information eintippen und direkt loslegen!</p>
 
 <form method="post">
-        <label for="Username">Benutzername </label>
-        <input id="Username" name="Username" type="text" placeholder="z.B. MaxMaster3314">
+        <label for="user_id">Benutzername </label>
+        <input id="Username" name="Username" type="text" placeholder="z.B. MaxMaster3314" required maxlength="16" minlength="8">
         <br>
-        <label for="Passwort">Passwort </label>
-        <input id="Passwort" name="Passwort" type="password" placeholder="Passwort">
+        <label for="Password">Passwort </label>
+        <input id="Password" name="Passwort" type="password" placeholder="Passwort" required maxlength="16" minlength="8">
          <br>
-        <label for="UserMail">Email-Adresse </label>
-        <input id="UserMail" name="UserMail" type="email" placeholder="z.B. meineMail@web.de">
+        <label for="Password2"> Passwort Bestätigen </label>
+        <input id="Password2" name="Passwort" type="password" placeholder="Passwort" required maxlength="16" minlength="8">
+        <br>
+        <label for="email">Email-Adresse </label>
+        <input id="email" name="UserMail" type="email" placeholder="z.B. meineMail@web.de" required maxlength="32" minlength="6">
          <br>
         <label>Email-Adresse bestätigen </label>
-        <input type="email" placeholder="z.B. meineMail@web.de">
+        <input type="email" placeholder="z.B. meineMail@web.de"  maxlength="32" minlength="6">
 
-    <button id="registrieren" type="submit" onclick="Platzhalter()" >Registrierung bestätigen</button>
+    <button id="registrieren"  type="submit" >Registrierung bestätigen</button>
 
-<!-- HIER NOCH PHP FUNKTION REINKNALLEN -->
 </form>
-
 </body>
 </html>
