949ae08655e25377fdacca59a6ac944a52f0bc60
Bauer <lucas.bauer@itc-studenten.de>
2019-12-04 15:47:06 +0100

    navbar bruder

--- AccountInformationen.php
+++ AccountInformationen.php
@@ -8,6 +8,14 @@
     <script src="js/custom.js"></script>
 </head>
 <body>
+<div>
+    <ul id="Navbar">
+        <li><a href="index.php" class="navbar">WhatToPlay?</a></li>
+        <li id="account"><a href="userPage.php" class="navbar"><img src="https://img.icons8.com/android/24/000000/user.png"></a></li>
+        <li id="registrieren"><a href="registrieren.php" class="navbar">Registrieren</a></li>
+        <li id="einloggen"><a href="einloggen.php" class="navbar">Einloggen</a></li>
+    </ul>
+</div>
 
 <p> Benutzername: [Dein Nutzername]</p>
 <p> E-Mail-Adresse: [Deine Mail]</p>
--- index.php
+++ index.php
@@ -16,16 +16,18 @@
 ?>
 </head>
 <body>
-
-<h1> WhatToPlay?</h1>
+<div>
+<ul id="Navbar">
+    <li><a href="index.php" class="navbar">WhatToPlay?</a></li>
+    <li id="account"><a href="userPage.php" class="navbar"><img src="https://img.icons8.com/android/24/000000/user.png"></a></li>
+    <li id="registrieren"><a href="registrieren.php" class="navbar">Registrieren</a></li>
+    <li id="einloggen"><a href="einloggen.php" class="navbar">Einloggen</a></li>
+</ul>
+</div>
+<h1>WhatToPlay?</h1>
 <p> Manchmal ist es garnicht so leicht, rauszufinden welches Spiel man spielen soll. <br>
     Diese Website soll dir dabei helfen, dich zurecht zu finden!<br>
     Ob schon erfahrener Zocker oder Neueinsteiger, diese Website ist für jeden.<br>
     Erstelle dir jetzt einen Account, um direkt loszulegen!</p>
-<button id="redirectRegistrieren" type="button" onclick="redirectZuRegistrieren()">Registrieren</button>
-
-<h2> Schon Mitglied?</h2>
-<button id="redirectEinloggen" type="button" onclick="redirectEinloggen()">Einloggen</button>
-
 </body>
 </html>
