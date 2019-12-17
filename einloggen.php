6485c041b43935fc0cfbe176b9bbf61ff8353ae1
Hsyn-Krnz <huseyin.kurnaz@itc-studenten.de>
2019-12-17 16:08:41 +0100

    // removed

--- einloggen.php
+++ einloggen.php
@@ -1,26 +1,25 @@
-<?php session_start();?>
-// <?php $_SESSION['name'] = "wert"; ?>
+<?php session_start(); ?>
+<?php $_SESSION['name'] = "wert"; ?>
 <?php
 function sessionstarten()
 {
-$thisPage = $_SERVER['PHP_SELF'];
-$pageNameArray = explode('/', $thisPage);
-$pageName = $pageNameArray[count($pageNameArray) - 1];
+    $thisPage = $_SERVER['PHP_SELF'];
+    $pageNameArray = explode('/', $thisPage);
+    $pageName = $pageNameArray[count($pageNameArray) - 1];
 //print "The name of this page is: $pageName<br/>";
-$nameItems = explode('.', $pageName);
-$sessionName = $nameItems[0];
+    $nameItems = explode('.', $pageName);
+    $sessionName = $nameItems[0];
 //print "The session name is $sessionName<br/>";
-if (!isset($_SESSION[$sessionName])) {
-$_SESSION[$sessionName] = 0;
+    if (!isset($_SESSION[$sessionName])) {
+        $_SESSION[$sessionName] = 0;
 //print "This is the first time you have visited this page<br/>";
-}
-else {
-$_SESSION[$sessionName]++;
-}
+    } else {
+        $_SESSION[$sessionName]++;
+    }
 //print "<h1>You have visited this page " . $_SESSION[$sessionName] ." times</h1>";
 }
-?>
 
+?>
 
 
 <!DOCTYPE html>
@@ -36,7 +35,8 @@ $_SESSION[$sessionName]++;
 <div>
     <ul id="Navbar">
         <li><a href="index.php" class="navbar">WhatToPlay?</a></li>
-        <li id="account"><a href="userPage.php" class="navbar"><img src="https://img.icons8.com/android/24/000000/user.png"></a></li>
+        <li id="account"><a href="userPage.php" class="navbar"><img
+                        src="https://img.icons8.com/android/24/000000/user.png"></a></li>
         <li id="registrieren"><a href="registrieren.php" class="navbar">Registrieren</a></li>
         <li id="einloggen"><a href="einloggen.php" class="navbar">Einloggen</a></li>
     </ul>
@@ -50,7 +50,7 @@ $_SESSION[$sessionName]++;
     <input id="Passwort" name="Passwort" type="password" placeholder="Passwort">
     <br>
 
-    <button type="submit" onclick=<?php sessionstarten()?>>Einloggen</button>
+    <button type="submit" onclick=<?php sessionstarten() ?>>Einloggen</button>
     <!-- HIER NOCH PHP FUNKTION REINKNALLEN -->
 </form>
 
